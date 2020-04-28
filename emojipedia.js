10c40af7c2ff0837911238b90332dd4bc3daaa16
Anjali Chauhan <61798282+anjc24@users.noreply.github.com>
2020-04-28 19:21:03 +0530

    Add files via upload

---
+++ emojipedia.js
@@ -0,0 +1,81 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id:5 ,
+    emoji:"😀" ,
+    name:"Grinning Face" ,
+    meaning: "A yellow face with simple, open eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general pleasure and good cheer or humor."
+  },
+  {
+    id: 6,
+    emoji:"😃",
+    name: "Grinning Face with Big Eyes",
+    meaning:"A yellow face with smiling eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general happiness and good-natured amusement."
+  },
+  {
+    id:7 ,
+    emoji: "😁",
+    name: "Beaming Face with Smiling Eyes",
+    meaning:"A yellow face with smiling eyes and full-toothed grin, as if saying Cheese! for the camera. Teeth may be smoothed-over or crosshatched. Often expresses a radiant, gratified happiness. Tone varies, including warm, silly, amused, or proud."
+  },
+  {
+    id:8 ,
+    emoji: "😉",
+    name: " Winking Face",
+    meaning:"A yellow face with a slight smile shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic."
+  },
+  {
+    id:9 ,
+    emoji: "🤗",
+    name: "Hugging Face",
+    meaning:"May be used to offer thanks and support, show love and care, or express warm, positive feelings more generally. Due to its hand gesture, often used to represent jazz hands, indicating such feelings as excitement, enthusiasm, or a sense of flourish or accomplishment."
+  },
+  {
+    id:10 ,
+    emoji: "😐",
+    name: "Neutral Face",
+    meaning:"A yellow face with simple, open eyes and a flat, closed mouth. Intended to depict a neutral sentiment but often used to convey mild irritation and concern or a deadpan sense of humor."
+  },
+  {
+    id:11 ,
+    emoji: "🤔",
+    name: "Thinking Face",
+    meaning:"A yellow face with furrowed eyebrows looking upwards with thumb and index finger resting on its chin. Intended to show a person pondering or deep in thought. Often used to question or scorn something or someone, as if saying Hmm, I don't know about that. Tone varies, including earnest, playful, puzzled, skeptical, and mocking."
+  },
+  {
+    id:12 ,
+    emoji: "😪",
+    name: "Sleepy Face",
+    meaning:"A yellow face with closed eyes, mouth slightly open, and a blue snot bubble coming from its nose. Snot bubbles indicate a character is tired or sleeping in anime or manga. Not to be confused with 😴 Sleeping Face "
+  },
+
+];
+
+export default emojipedia;
